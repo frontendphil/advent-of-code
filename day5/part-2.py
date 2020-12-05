aff069dfeb8c3c32ad337a57e496a15051f62624
Philipp Giese <giese.philipp+git@gmail.com>
2020-12-05 15:48:35 +0100

    day 5 part 2

---
+++ day5/part-2.py
@@ -0,0 +1,47 @@
+import re
+import math
+
+col_row_matcher = re.compile(r'^(.{7})(.{3})$')
+
+
+def disect_partition(partition, lower, upper, min, max):
+    if(len(partition) == 1):
+        return max if partition == upper else min
+
+    if(partition.startswith(lower)):
+        return disect_partition(partition[1:], lower, upper, min, min + math.floor((max - min) / 2))
+
+    return disect_partition(partition[1:], lower, upper, min + math.ceil((max - min) / 2), max)
+
+
+def get_column_id(partition):
+    return disect_partition(partition, lower="L", upper="R", min=0, max=7)
+
+
+def get_row_id(partition, min=0, max=127):
+    return disect_partition(partition, lower="F", upper="B", min=0, max=127)
+
+
+def get_seat_id(partition):
+    match = col_row_matcher.match(partition)
+
+    row_partition = match.group(1)
+    column_partition = match.group(2)
+
+    return get_row_id(row_partition) * 8 + get_column_id(column_partition)
+
+
+def find_open_spot(pairs):
+    for left, right in pairs:
+        if(right - left != 1):
+            return left + 1
+
+
+if __name__ == "__main__":
+    with open('./input.txt') as f:
+        seat_ids = sorted([get_seat_id(line) for line in f.readlines()])
+
+        pairs = [(left, seat_ids[index + 1])
+                 for index, left in enumerate(seat_ids[:-1])]
+
+        print(find_open_spot(pairs))
