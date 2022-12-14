e90831f749593f70ad0a1605cffd4e20cf760a1c
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-12-14 09:56:53 +0100

    solve first part

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").expect("Could not read inputs");
+    let lines = contents.split("\n");
+
+    let mut calories = Vec::new();
+    let mut current = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            calories.push(current);
+            current = 0
+        } else {
+            current += line.parse::<i32>().unwrap();
+        }
+    }
+
+    let max = calories.iter().max().unwrap();
+
+    println!("Max calories: {max}");
+
+    calories.sort();
+
+    let top: i32 = calories[0..3].iter().sum();
+
+    println!("Top 3: {top}");
 }
