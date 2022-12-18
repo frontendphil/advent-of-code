be0fa0b9f6a74be8504c49f6a8dc83470f31f8b7
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-12-18 20:14:03 +0100

    solve day 04

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -1,11 +1,16 @@
-use std::{fs::read_to_string, ops::Range};
+use std::{fs::read_to_string, ops::Range, str::Lines};
 
 fn main() {
     let contents = read_to_string("input.txt").unwrap();
 
+    part_one(contents.lines());
+    part_two(contents.lines());
+}
+
+fn part_one(contents: Lines) {
     let mut overlaps = 0;
 
-    for assignments in contents.lines() {
+    for assignments in contents {
         let mut iterator = assignments.split(",");
 
         let first = iterator.next().unwrap();
@@ -20,6 +25,30 @@ fn main() {
     }
 
     println!("Sum {overlaps}");
+}
+
+fn part_two(contents: Lines) {
+    let mut count = 0;
+
+    for assignments in contents {
+        let mut iterator = assignments.split(",");
+
+        let first = iterator.next().unwrap();
+        let second = iterator.next().unwrap();
+
+        let a = get_range(first);
+        let b = get_range(second);
+
+        if overlaps(&a, &b) || overlaps(&b, &a) {
+            count += 1;
+        }
+    }
+
+    println!("Overlaps: {count}");
+}
+
+fn overlaps(a: &Range<i32>, b: &Range<i32>) -> bool {
+    return a.end >= b.start && a.end <= b.end;
 }
 
 fn contains(a: &Range<i32>, b: &Range<i32>) -> bool {
