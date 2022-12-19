d9b1fa25e1ebd07d693c2e255e4e0d6a10662f27
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-12-19 23:06:08 +0100

    part two

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,16 +1,20 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, str::Lines};
 
 use regex::Regex;
 
 fn main() {
     let contents = read_to_string("input.txt").unwrap();
 
+    part_one(contents.lines());
+    part_two(contents.lines());
+}
+
+fn part_one(contents: Lines) {
     let re = Regex::new(r"move (?P<times>\d+) from (?P<from>\d+) to (?P<to>\d+)").unwrap();
     let mut stacks = init_stacks();
 
-    for line in contents.lines() {
+    for line in contents {
         if !re.is_match(line) {
-            println!("does not match");
             continue;
         }
 
@@ -20,12 +24,8 @@ fn main() {
         let from = matches["from"].parse::<usize>().unwrap() - 1;
         let to = matches["to"].parse::<usize>().unwrap() - 1;
 
-        println!("move {times} from {from} to {to}");
-
         for _i in 0..times {
             let item = get_item(&mut stacks, from);
-
-            println!("item {item}");
 
             insert_item(&mut stacks, to, item);
         }
@@ -34,7 +34,33 @@ fn main() {
     let top_values = get_top_values(&mut stacks);
     let top_string = top_values.map(|x| x.to_string()).join("");
 
-    println!("Top {top_string}")
+    println!("Top (part one) {top_string}")
+}
+
+fn part_two(contents: Lines) {
+    let re = Regex::new(r"move (?P<times>\d+) from (?P<from>\d+) to (?P<to>\d+)").unwrap();
+    let mut stacks = init_stacks();
+
+    for line in contents {
+        if !re.is_match(line) {
+            continue;
+        }
+
+        let matches = re.captures(line).unwrap();
+
+        let times = matches["times"].parse::<usize>().unwrap();
+        let from = matches["from"].parse::<usize>().unwrap() - 1;
+        let to = matches["to"].parse::<usize>().unwrap() - 1;
+
+        let items = get_items(&mut stacks, from, times);
+
+        insert_items(&mut stacks, to, items);
+    }
+
+    let top_values = get_top_values(&mut stacks);
+    let top_string = top_values.map(|x| x.to_string()).join("");
+
+    println!("Top (part two) {top_string}")
 }
 
 fn get_top_values(stacks: &mut [Vec<char>; 9]) -> [char; 9] {
@@ -51,10 +77,26 @@ fn get_top_values(stacks: &mut [Vec<char>; 9]) -> [char; 9] {
     ];
 }
 
+fn get_items(stacks: &mut [Vec<char>; 9], index: usize, count: usize) -> Vec<char> {
+    let source = &mut stacks[index];
+
+    return source.drain(0..count).collect();
+}
+
 fn get_item(stacks: &mut [Vec<char>; 9], index: usize) -> char {
     let source = &mut stacks[index];
 
     return source.remove(0);
+}
+
+fn insert_items(stacks: &mut [Vec<char>; 9], index: usize, mut items: Vec<char>) {
+    let target = &mut stacks[index];
+
+    items.reverse();
+
+    for item in items {
+        target.insert(0, item);
+    }
 }
 
 fn insert_item(stacks: &mut [Vec<char>; 9], index: usize, item: char) {
