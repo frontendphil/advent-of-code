09d67b94482c72d0d8c6d02fe23c28b88172a713
Philipp Giese <giese.philipp+git@gmail.com>
2022-12-20 11:11:13 +0100

    part two

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -4,20 +4,24 @@ fn main() {
     let contents = read_to_string("input.txt").unwrap();
 
     for line in contents.lines() {
-        let index = get_marker_index(line);
+        let start_of_packet = get_marker_index(line, 4);
 
-        println!("Index {index}");
+        println!("start of packet {start_of_packet}");
+
+        let start_of_message = get_marker_index(line, 14);
+
+        println!("start of message {start_of_message}");
     }
 }
 
-fn get_marker_index(line: &str) -> i32 {
+fn get_marker_index(line: &str, desired_size: usize) -> i32 {
     let mut current_marker: Vec<char> = Vec::new();
     let mut index = -1;
 
     for char in line.chars() {
         index += 1;
 
-        if !enough_elements(&mut current_marker) {
+        if !enough_elements(&mut current_marker, desired_size) {
             println!("Add initial {char}");
             current_marker.push(char);
         } else if is_marker(&mut current_marker) {
@@ -38,8 +42,8 @@ fn cycle_marker(marker: &mut Vec<char>, char: char) {
     println!("Remove {removed} and inserted {char}");
 }
 
-fn enough_elements(marker: &mut Vec<char>) -> bool {
-    return marker.len() == 4;
+fn enough_elements(marker: &mut Vec<char>, desired_size: usize) -> bool {
+    return marker.len() == desired_size;
 }
 
 fn is_marker(marker: &mut Vec<char>) -> bool {
